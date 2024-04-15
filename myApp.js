90d6aec6ff59bb5a388157bba633c4f6e83620c9
Alex Mwaniki <lekko254@gmail.com>
2024-04-15 20:45:45 +0000

    .env

--- myApp.js
+++ myApp.js
@@ -3,13 +3,15 @@ let app = express();
 require ('dotenv').config();
 
 
-app.get('/json', (req, res) => {
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        res.json({"message" : "HELLO JSON"});
-    } else {
-        res.json({"message" : "hello json"});
-    }
-})
+// app.get('/json', (req, res) => {
+//     if (process.env.MESSAGE_STYLE === "uppercase") {
+//         res.json({"message": "hello json".toUpperCase()});
+//     } else {
+//         res.json({"message": "Hello json"});
+//     }
+// })
+
+
 
 
 

f9ad84348737355c797921a43de9a4d5fc4c2296
Alex Mwaniki <lekko254@gmail.com>
2024-04-15 20:36:35 +0000

    json

--- myApp.js
+++ myApp.js
@@ -1,9 +1,15 @@
 let express = require('express');
 let app = express();
+require ('dotenv').config();
 
 
-
-
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({"message" : "HELLO JSON"});
+    } else {
+        res.json({"message" : "hello json"});
+    }
+})
 
 
 

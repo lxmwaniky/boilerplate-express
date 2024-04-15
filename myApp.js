497dc81528ee316ccc3750d691a15fd0d01270aa
Alex Mwaniki <lekko254@gmail.com>
2024-04-15 21:37:23 +0000

    echo

--- myApp.js
+++ myApp.js
@@ -11,12 +11,24 @@ require ('dotenv').config();
 //     }
 // })
 
-app.use(function middleware(req, res, next) {
-    console.log(req.method + " " + req.path + "json - " + req.ip);
-    next();
-})
+// app.use(function middleware(req, res, next) {
+//     console.log(req.method + " " + req.path + "json - " + req.ip);
+//     next();
+// })
 
+// app.get("/now", (req, res, next) => {
+//     req.time = new Date().toString();
+//     next();
+//   }, (req, res) => {
+//     res.send({time: req.time});
+//   });
 
+app.get("/:word/echo", (req, res) => {
+    const { word } = req.params;
+    res.json({
+      echo: word
+    });
+  });
 
 
 

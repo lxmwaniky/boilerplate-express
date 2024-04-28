2f2ca06bd5183a416fc3dcf4b6a33d1c81ddf075
Alex Mwaniki <lekko254@gmail.com>
2024-04-28 16:29:58 +0000

    done

--- myApp.js
+++ myApp.js
@@ -1,76 +1,25 @@
 let express = require('express');
 let app = express();
-require ('dotenv').config();
+require('dotenv').config();
 var bodyParser = require("body-parser");
 
+// Configure body-parser middleware to parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/json', (req, res) => {
-//     if (process.env.MESSAGE_STYLE === "uppercase") {
-//         res.json({"message": "hello json".toUpperCase()});
-//     } else {
-//         res.json({"message": "Hello json"});
-//     }
-// })
-
-// app.use(function middleware(req, res, next) {
-//     console.log(req.method + " " + req.path + "json - " + req.ip);
-//     next();
-// })
-
-// app.get("/now", (req, res, next) => {
-//     req.time = new Date().toString();
-//     next();
-//   }, (req, res) => {
-//     res.send({time: req.time});
-//   });
-
-// app.get("/:word/echo", (req, res) => {
-//     const { word } = req.params;
-//     res.json({
-//       echo: word
-//     });
-//   });
-
+// GET handler for /name route
 app.get("/name", function(req, res) {
-    var firstName = req.query.first;
-    var lastName = req.query.last;
+  var firstName = req.query.first;
+  var lastName = req.query.last;
 
-    var { first: firstName, last: lastName } = req.query;
-
-    res.json({
-      name: `${firstName} ${lastName}`
-    });
+  res.json({
+    name: `${firstName} ${lastName}`
   });
+});
 
-
+// POST handler for /name route
 app.post("/name", function(req, res) {
-    var string = req.body.first + " " + req.body.last;
-    res.json({ name: string });
-  });
+  var string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
